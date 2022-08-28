408ecf0e25d7d99c94724c4f6ff2c0c62989c917
XeniaPhe <m.tunahanbas90@gmail.com>
2022-08-28 17:02:42 +0300

    Latest Version

--- Angles/obj/Release/netcoreapp3.1/Angles.AssemblyInfo.cs
+++ Angles/obj/Release/netcoreapp3.1/Angles.AssemblyInfo.cs
@@ -15,11 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("This library is dedicated to easily and quickly handling and manipulating differe" +
     "nt types and units of angles")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.6.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.6")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.7.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Angles.Net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Angles")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.6.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.7.0")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/XeniaPhe/Angles.Net")]
 [assembly: System.Resources.NeutralResourcesLanguageAttribute("en-001")]
 
